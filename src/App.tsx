0418400f53033f6f9b13c048ef22b76877d8be2a
Tudor <tudor@Tudors-MacBook-Pro.local>
2026-01-08 00:14:30 +0200

    Add auto scroll-to-top on route changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import LandingPage from './pages/LandingPage'
 import AboutPage from './pages/AboutPage'
 import SolutionPage from './pages/SolutionPage'
 import CookieBanner from './components/CookieBanner'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutPage />} />
