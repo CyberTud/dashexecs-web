10fd4337c34f372bcbc13bb0bccdffd67b7a363c
Tudor <tudor@Tudors-MacBook-Pro.local>
2026-01-07 20:18:22 +0200

    Commit pending changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
-import { useState, useCallback } from 'react'
 import LandingPage from './pages/LandingPage'
-import SplashVideo from './components/SplashVideo'
 
 function App() {
-  const [showSplash, setShowSplash] = useState(true)
-  const handleFinished = useCallback(() => setShowSplash(false), [])
-
-  if (showSplash) {
-    return <SplashVideo onFinished={handleFinished} />
-  }
   return <LandingPage />
 }
 
